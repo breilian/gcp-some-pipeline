bf85a4d8faf1074fc1632b15b547ec115435c184
breilian <lexa.br@gmail.com>
2021-04-17 15:43:22 +0300

    initial commit

---
+++ src/main/java/gcp/somepipeline/SomePipelineApplication.java
@@ -0,0 +1,13 @@
+package gcp.somepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomePipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gcp/somepipeline/SomePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package gcp.somepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
