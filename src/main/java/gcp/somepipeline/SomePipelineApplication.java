f349b772720d2ce5d7794e8515be7ecb302d0aac
breilian <lexa.br@gmail.com>
2021-04-17 15:57:02 +0300

    working example

--- src/main/java/gcp/somepipeline/SomePipelineApplication.java
+++ src/main/java/gcp/somepipeline/SomePipelineApplication.java
@@ -2,12 +2,20 @@ package gcp.somepipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SomePipelineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SomePipelineApplication.class, args);
+	}
+
+	@GetMapping("/check")
+	public String check() {
+		return "It works.";
 	}
 
 }
