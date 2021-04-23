2342af1960efd1f7458677ee090bb93b96f4d793
breilian <lexa.br@gmail.com>
2021-04-23 19:41:26 +0300

    test trigger by commit 1

--- src/main/java/gcp/somepipeline/controller/ModelController.java
+++ src/main/java/gcp/somepipeline/controller/ModelController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v5")
+@RequestMapping("api/v1")
 public class ModelController {
 
     @Autowired
